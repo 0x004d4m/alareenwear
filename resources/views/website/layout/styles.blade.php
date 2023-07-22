598106ff58b579ac2940acbf30d4fca4ffc5124d
Adam Sabbah <Adam Sabbah>
2023-07-22 16:42:29 +0300

    updates

--- resources/views/website/layout/styles.blade.php
+++ resources/views/website/layout/styles.blade.php
@@ -5,7 +5,7 @@
 <link rel="shortcut icon" href="favicon.png">
 
 <meta name="description" content="Al-Areen Wear Ltd. was established in 2005 and has been operating at its current location since 2009. With 14 years of experience and technical expertise in global garment manufacturing, our factory is located in the Deir Aby Said, Kora restrict, Irbid provinces of Jordan, close to the Haifa Port for exports. We are aware of the competitive market and strive for quick and flexible turnaround times while meeting customer quality requirements. We believe in fostering good relationships between management and employees for a shared vision and goal."/>
-<meta name="keywords" content="al-areen, alareen, areen, alareen wear, wear, clothes, Al-Areen Wear Ltd, global garment manufacturing, Deir Aby Said, clothes in Irbid, clothes in jordan, clothes supplier in jordan" />
+<meta name="keywords" content="al-areen, alareen, areen, alareen wear, wear, clothes, Al-Areen Wear Ltd, global garment manufacturing, Deir Aby Said, clothes in Irbid, clothes in jordan, clothes supplier in jordan, العرين لصناعة الالبسة" />
 
 <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
 
