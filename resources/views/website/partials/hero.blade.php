b65f2a90a163452a33a098eee17cfbeafb62ada3
Adam Sabbah <Adam Sabbah>
2023-07-22 16:03:16 +0300

    updates

--- resources/views/website/partials/hero.blade.php
+++ resources/views/website/partials/hero.blade.php
@@ -1,5 +1,5 @@
 <div class="hero-wrap" style="background-image: url('{{url($Hero->image)}}'); background-attachment:fixed;">
-    <div class="overlay"></div>
+    <div class="overlay" style="background: linear-gradient(to right, #6a00ff 0%, #ffd900 100%);"></div>
     <div class="container">
         <div class="row no-gutters slider-text align-items-center justify-content-center" data-scrollax-parent="true">
             <div class="col-md-8 ftco-animate text-center">
