bd5f00d2f7087ea8af990e22a60da55bc7a7217d
Adam Sabbah <Adam Sabbah>
2023-07-22 13:49:18 +0300

    updates

--- resources/views/website/partials/services.blade.php
+++ resources/views/website/partials/services.blade.php
@@ -4,7 +4,7 @@
             @foreach ($Services as $Service)
                 <div class="col-md-4 align-self-stretch ftco-animate">
                     <div class="media block-6 services p-3 py-4 d-block text-center">
-                        <div class="icon d-flex justify-content-center align-items-center mb-3"><img src="{{$Service->image}}"></div>
+                        <div class="icon d-flex justify-content-center align-items-center mb-3"><img src="{{$Service->image}}" class="img-fluid"></div>
                         <div class="media-body px-3">
                             <h3 class="heading">{{$Service->title}}</h3>
                             <p>{{$Service->description}}</p>
