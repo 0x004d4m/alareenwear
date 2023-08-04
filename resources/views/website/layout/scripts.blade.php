79deb42f57960c25455f529cac0d3d971a646f3a
Adam Sabbah <Adam Sabbah>
2023-08-04 13:35:15 +0300

    updates

--- resources/views/website/layout/scripts.blade.php
+++ resources/views/website/layout/scripts.blade.php
@@ -12,6 +12,6 @@
 <script src="{{url('template/js/bootstrap-datepicker.js')}}"></script>
 <script src="{{url('template/js/jquery.timepicker.min.js')}}"></script>
 <script src="{{url('template/js/scrollax.min.js')}}"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+{{-- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script> --}}
 <script src="{{url('template/js/google-map.js')}}"></script>
 <script src="{{url('template/js/main.js')}}"></script>
