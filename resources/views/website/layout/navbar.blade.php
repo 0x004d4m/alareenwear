bc27bc9f60f1c14185aed5490f6e5720fa846cbc
Adam Sabbah <Adam Sabbah>
2023-08-04 13:17:57 +0300

    updates

--- resources/views/website/layout/navbar.blade.php
+++ resources/views/website/layout/navbar.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar navbar-expand-lg navbar-light ftco_navbar bg-light ftco-navbar-light" id="ftco-navbar">
     <div class="container" dir="ltr">
-        <a class="navbar-brand" href="#" height="10%" width="30%"><img src="{{ url('logo/logo.png') }}" height="10%" width="30%"></a>
+        <a class="navbar-brand" href="{{ url('logo/logo.png') }}"><img src="{{ url('logo/logo.png') }}" height="10%" width="30%"></a>
         <button class="navbar-toggler float-right" type="button" data-toggle="collapse" data-target="#ftco-nav"
             aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="oi oi-menu"></span> {{__('content.menu')}}
+            <span class="oi oi-menu"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="ftco-nav">
@@ -16,7 +16,7 @@
                 <li class="nav-item"><a href="gallery" class="nav-link">{{__('content.gallery')}}</a></li>
                 <li class="nav-item"><a href="contact" class="nav-link">{{__('content.contact')}}</a></li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="javascript:void(0)" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {{Backpack\LangFileManager\app\Models\Language::where('abbr',Session::get('locale'))->first()->native}}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
