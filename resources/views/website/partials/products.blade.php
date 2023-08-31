4b4d62daec37f45e3f6824a5dcd0e68599312071
Adam Sabbah <Adam Sabbah>
2023-08-31 13:21:49 +0300

    updates

--- resources/views/website/partials/products.blade.php
+++ resources/views/website/partials/products.blade.php
@@ -8,12 +8,7 @@
         <div class="row text-center">
             @foreach ($Products as $Product)
                 <div class="col-md-4 ftco-animate mb-3">
-                    <div class="course align-self-stretch">
-                        <a href="#" class="img"><img src="{{ url($Product->image) }}" class="img-fluid"></a>
-                        {{-- <div class="text p-4">
-                            <h3 class="mb-3"><a href="#">{{$Product->name}}</a></h3>
-                        </div> --}}
-                    </div>
+                    <img src="{{ url($Product->image) }}" class="img-fluid">
                 </div>
             @endforeach
         </div>
